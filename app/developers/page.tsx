3c6dbabb711c0b531ac67a24336fd8e392d10642
cbckweb-live <cbckweb@gmail.com>
2026-06-27 21:41:31 +0530

    Fix developers page cookie mutation crash

--- app/developers/page.tsx
+++ app/developers/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { supabase } from "@/lib/supabase";
-import { cookies } from "next/headers";
+
 import { createSupabaseServerClient } from "@/lib/supabase-server";
 
 
