af449021930d5fb4266e33d3d1cdd8d7d990dd0a
cbckweb-live <cbckweb@gmail.com>
2026-07-06 19:38:20 +0530

    Homepage edit content

--- app/page.tsx
+++ app/page.tsx
@@ -151,16 +151,21 @@ export default async function HomePage() {
             <br /> Youth Ministry
           </h1>
           <p className="text-[#231F1E]/80 leading-relaxed">
-            Welcome to the Youth Ministry of Chakhesang Baptist Church, Kohima—a community of young believers growing together in faith, fellowship, and service under the spiritual guidance of the Church.
+            Welcome to the Youth Ministry of Chakhesang Baptist Church, Kohima- 
+            a community of young believers growing together in faith, fellowship, 
+            and service under the spiritual guidance of the Church.
+          </p>
+          <br/>
+          <p className="text-[#231F1E]/80 leading-relaxed"> 
+          Established in 1968, wiyh a growing family of over 1,000 members, 
+          we continue to cultivate a Christ-centred community where worship, discipleship, fellowship,
+          leadership, creativity, service, and spiritual care to shape every aspect of our journey together.
           </p>
           <br/>
           <p className="text-[#231F1E]/80 leading-relaxed">
-          Our emblem beautifully reflects who we are and what we aspire to become. At its centre stands the Cross of Jesus Christ, proclaiming His victory over the world and affirming that He alone is the Way, the Truth, and the Life. Established in 1968, our ministry has been committed to nurturing generations of young people to become mature disciples of Jesus Christ. Today, with more than 1,000 members, we continue to cultivate a Christ-centred community where worship, discipleship, fellowship, leadership, creativity, service, and spiritual care shape every aspect of our journey together.
-          </p>
-          <br/>
-          <p className="text-[#231F1E]/80 leading-relaxed">
-          Whether you are visiting for the first time, seeking a place to belong, or looking to deepen your walk with God, we warmly welcome you to be part of this growing family. Together, let us grow in faith, stand firm in Christ, and faithfully serve God and our community. 
-
+          Whether you are visiting for the first time, seeking a place to belong,
+           or looking to deepen your walk with God, we warmly welcome you to be part of this growing family. 
+           Together, let us grow in faith, stand firm in Christ, and faithfully serve God and our community. 
           </p>
           <br/>
         </div>
