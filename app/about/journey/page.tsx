cdbb7245e4cf13ea16f5133c41c81b3953b418e7
cbckweb-live <cbckweb@gmail.com>
2026-07-06 14:22:08 +0530

    Homepage edit

--- app/about/journey/page.tsx
+++ app/about/journey/page.tsx
@@ -3,7 +3,16 @@ export default function JourneyPage() {
     <main className="px-8 py-16 max-w-2xl mx-auto">
       <h1 className="font-display text-3xl mb-4">Journey</h1>
       <p className="text-[#231F1E]/80 leading-relaxed">
-        Our journey as a community will be written here.
+        The Youth Ministry of Chakhesang Baptist Church, Kohima was established in 1968, eight years after the establishment of the church. Since its inception, the ministry has served as a platform for nurturing young believers in their spiritual journey, equipping them for Christian leadership, and encouraging active participation in the life and mission of the church.
+
+      </p>
+      <p className="text-[#231F1E]/80 leading-relaxed">
+        Through the decades, the ministry has grown alongside the church,and has impacted many lives. From a handful of a hundred members, today, the ministry has grown into one of the largest departments within the church, with more than 1,000 members. In 2018, the ministry joyfully celebrated its Golden Jubilee, marking fifty years of God&apos;s faithfulness, spiritual growth, and dedicated service. The celebration not only reflected on the rich legacy of the past but also reaffirmed the ministry&apos;s commitment to raising Christ-centred leaders for the future. 
+
+      </p>
+     <p className="text-[#231F1E]/80 leading-relaxed">
+        To better minister to the varying needs of young people, the ministry includes a dedicated group known as Mathetes, specifically for Class XI and XII students (approximately 16–18 years of age). This transitional group focuses on discipleship, spiritual formation, mentoring, and preparing young believers for greater involvement in the wider Youth Ministry.  The ministry, having evolved with the call to inspire young people to live Christ-centred lives and equipped to serve God and the society faithfully, continues to widen scope of outreach with purpose solely being transformation and to be a chain as a living witness of God’s faithfulness and discipleship. 
+
       </p>
     </main>
   );
