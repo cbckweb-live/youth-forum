dcbf34398106c0b0bf791ea7764e96ce7a70ca04
cbckweb-live <cbckweb@gmail.com>
2026-07-07 21:54:32 +0530

    maps

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -125,6 +125,7 @@ export default function Footer() {
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.891652834068!2d94.10877407415414!3d25.674886112189345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3746210abc33ddf3%3A0x366e970c18b6490e!2sChakhesang%20Baptist%20Church!5e0!3m2!1sen!2sus!4v1782318995927!5m2!1sen!2sus"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
