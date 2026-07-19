e274b072b0298d010d67606452f4dc5b3b0464a0
cbckweb-live <cbckweb@gmail.com>
2026-07-19 22:24:54 +0530

    bypass error fixing

--- proxy.ts
+++ proxy.ts
@@ -25,7 +25,7 @@ export async function proxy(request: NextRequest) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, 
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
     });
     return redirectResponse;
